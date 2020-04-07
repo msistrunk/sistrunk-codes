f69fa84b470dab2f1deae3402fc385ffb8fb2672
Mitchell Sistrunk <sistrunkmitchell@gmail.com>
2020-04-07 01:21:53 -0500

    Coming soon

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,53 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+  <v-app id="inspire" class="display-4">
+    <div class="container">
+      <h1>COMING SOON</h1>
+      <div class="text-center ma-12">
+        <v-progress-circular
+          :indeterminate="true"
+          :size="60"
+          color="light-blue"
+        ></v-progress-circular>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    </div>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+  // components: {
+  //   HelloWorld,
+  // },
 
   data: () => ({
     //
   }),
+
+  created () {
+    this.$vuetify.theme.dark = true
+  },
 };
 </script>
+
+<style scoped>
+.container {
+  font-size:16px;
+  top:20%;
+  position:absolute;  
+  color:#FFFFFF;
+  text-align:center;
+}
+
+h1 {
+  font-size:3.5em;
+  font-weight:100;
+  letter-spacing:.5em;
+  text-shadow:0px 0px 20px #6EC3EC;
+  color:#6EC3EC;
+  display:inline-block;
+}
+</style>
